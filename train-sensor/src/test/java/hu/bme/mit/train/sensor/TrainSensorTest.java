459f0da33b232511976a1663bc137c9ccb0d5aed
traderain <hambalko.bence@gmail.com>
2020-03-02 15:57:23 +0100

    Add guava test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,8 +3,16 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Map;
 
 public class TrainSensorTest {
 
@@ -21,5 +29,18 @@ public class TrainSensorTest {
     @Test
     public void FancyTestStub() {
         assertEquals(0,0);
+
+        Map items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
+
+        items.entrySet()
+                .stream()
+                .forEach(System.out::println);
+
+        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi",
+                "mandarin", "date", "quince");
+
+        for (String fruit: fruits) {
+            System.out.println(fruit);
+        }
     }
 }
