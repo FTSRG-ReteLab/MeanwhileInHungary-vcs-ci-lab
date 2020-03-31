95470b2085bb78485f9cae5080bc8534c22b7ba6
hhenrikk <hhenrik@gmx.com>
2020-03-31 18:41:49 +0200

    unit testing 1 exercise, new feature implementation, alarm feature

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,6 +22,9 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		if(speedLimit<0 || 500<speedLimit || speedLimit<0.5*this.speedLimit){
+			user.setAlarmState(true);
+		}
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 	}
