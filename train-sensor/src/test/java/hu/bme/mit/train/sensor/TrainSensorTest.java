24ec4262a561b48fa58f84bcaee34dd6815578ff
hhenrikk <hhenrik@gmx.com>
2020-03-31 20:06:54 +0200

    tests for TrainSensor

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,43 +4,67 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Map;
-
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import java.util.List;
-import java.util.Map;
+
+import hu.bme.mit.train.interfaces.*;
+import hu.bme.mit.train.sensor.TrainSensorImpl;
 
 public class TrainSensorTest {
 
+    TrainController controller;
+    TrainUser user;
+    TrainSensorImpl sensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(controller,user);
     }
 
+    /* Test, whether alarm state will be triggered, as specified, in case the speed limit is set negative.
+     * 1: User not in alarm state, speed limit is at the default 10
+     * 2: overrideSpeedLimit(-1)
+     * 3: User must be in alarm state
+     */
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void AlarmFeatureTest_Negative() {
+        sensor.overrideSpeedLimit(-1);
+	verify(user,times(1)).setAlarmState(true);        
     }
-    
+
+    /* Test, whether the alarm state will be triggered, as specified, in case the speed limit is set above 500.
+     * 1: User not in alarm state, speed limit is at the default 10
+     * 2: overrideSpeedLimit(501)
+     * 3: User must be in alarm state
+     */
     @Test
-    public void FancyTestStub() {
-        assertEquals(0,0);
-
-        Map items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
-
-        items.entrySet()
-                .stream()
-                .forEach(System.out::println);
-
-        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi",
-                "mandarin", "date", "quince");
-
-        for (String fruit: fruits) {
-            System.out.println(fruit);
-        }
+    public void AlarmFeatureTest_Above500() {
+        sensor.overrideSpeedLimit(501);
+	verify(user,times(1)).setAlarmState(true);
     }
+
+    /* Test, whether the alarm state will be triggered, as specified, in case the speed limit is set below half of its previous value.
+     * 1: User not in alarm state, speed limit is at the default 10
+     * 2: overrideSpeedLimit(4)
+     * 3: User must be in alarm state
+     */
+    @Test
+    public void AlarmFeatureTest_LessThanHalf() {
+        sensor.overrideSpeedLimit(4);
+	verify(user,times(1)).setAlarmState(true);
+    }
+
+    /* Test, whether the alarm state will not be triggered, as specified, in case the speed limit is set from 10 to 20.
+     * 1: User not in alarm state, speed limit is at the default 10
+     * 2: overrideSpeedLimit(20)
+     * 3: User must not be in alarm state
+     */
+    @Test
+    public void AlarmFeatureTest_Normal() {
+        sensor.overrideSpeedLimit(20);
+	verify(user,times(0)).setAlarmState(true);
+    }
+
 }
